64c29bf12d283fa3bc785ceb93f7a13879ac4502
pvallet <luap.vallet@gmail.com>
2024-10-02 07:14:42 +0200

    Fixed some typos

--- src/main.cpp
+++ src/main.cpp
@@ -33,10 +33,10 @@ int main(int /*argc*/, char* /*argv*/[]) {
     // Set render color to red ( background will be rendered in this color )
     SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
 
-    // Clear winow
+    // Clear window
     SDL_RenderClear( renderer );
 
-    // Creat a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
+    // Create a rect at pos ( 50, 50 ) that's 50 pixels wide and 50 pixels high.
     SDL_Rect r;
     r.x = 50;
     r.y = 50;
